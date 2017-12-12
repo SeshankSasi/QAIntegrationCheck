9c98922895efa35356d5f16611fdef22c53ccf71
SeshankSasi <32571411+SeshankSasi@users.noreply.github.com>
2017-12-12 15:17:17 +0530

    2nd commit
    2nd commit

---
+++ MavenProject/src/test/java/MavenPack/MavenProject/NewTest.java
@@ -0,0 +1,21 @@
+package MavenPack.MavenProject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeTest;
+//import org.testng.annotations.Test;
+
+
+public class NewTest {
+  
+  @BeforeTest
+  public void start(){
+		System.setProperty("webdriver.gecko.driver", "D:\\SeshankLPBackUP2017\\Ofc Stuff\\Testing Stuff\\Selenium\\Selenium drivers\\geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://store.demoqa.com//");
+        driver.manage().window().fullscreen();
+
+      
+  }
+}
